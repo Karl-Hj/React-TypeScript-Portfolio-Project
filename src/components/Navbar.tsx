77d55cdb305965f5eb9f685e2c90c99f975f9d10
b19karhj <sc2rena@gmail.com>
2024-10-26 18:14:45 +0200

    Skeleton navbar
    Implemented a navbar with dropdown. Adding more styling later on.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,26 @@
 import { Outlet } from "react-router-dom";
+
+import { useToggle } from "./useToggle";
 import "./navBar.css";
 
 //Navbar that also loads in the container page
 
 export function Navbar() {
+  const [isActive, ToggleDropdown] = useToggle(false);
+
+  const dropDown = isActive ? "main-nav__item active" : "main-nav__item";
+
   return (
     <>
-      <header>
-        <nav>
-          <ul>
-            <li>test1</li>
-            <li>test2</li>
-            <li>test3</li>
+      <header className="main-header">
+        <nav className="main-nav">
+          <ul className="main-nav__items ">
+            <li className={dropDown}>test1</li>
+            <li className={dropDown}>test2</li>
+            <li className={dropDown}>test3</li>
+            <li className="main-nav__menu-icon" onClick={ToggleDropdown}>
+              <span className="main-nav__icon-bars"></span>
+            </li>
           </ul>
         </nav>
       </header>
---
+++ src/components/useToggle.ts
@@ -0,0 +1,10 @@
+import { useState } from "react";
+
+export function useToggle(initialValue: boolean): [boolean, () => void] {
+  const [value, setValue] = useState(initialValue);
+  function toggle() {
+    setValue((currentValue) => !currentValue);
+    console.log("toggle");
+  }
+  return [value, toggle];
+}
